5ec51973654eeda9427755d096f0ee145c8491ab
Eslam Saeed <Eslamsa3ed50@gmail.com>
2018-12-03 21:53:16 +0200

    chat data class

---
+++ chat-app/src/app/chats.ts
@@ -0,0 +1,8 @@
+export class Chats {
+    constructor(
+        public id: number,
+        public name: string,
+        public img:string,
+        public time:string,
+        public chats:string) { }
+}
