287fae8e235a782966ff2eea3ef25ad3f2b1c418
Eslam Saeed <Eslamsa3ed50@gmail.com>
2018-12-03 21:51:22 +0200

    improvments on (chats-contact-search)list

--- chat-app/src/app/chat-list/chat-list.component.ts
+++ chat-app/src/app/chat-list/chat-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Chats} from '../chats';
 
 @Component({
   selector: 'app-chat-list',
@@ -8,7 +9,15 @@ import { Component, OnInit } from '@angular/core';
 export class ChatListComponent implements OnInit {
 
   constructor() { }
-
+  myChats = [
+    new Chats(1, 'Olivia','http://placehold.it/45','10:00AM','hello this is test message'),
+    new Chats(2, 'Jake Oliver','http://placehold.it/45','10:00AM','hello this is test message'),
+    new Chats(3, 'Windstorm','http://placehold.it/45','10:00AM','hello this is test message'),
+    new Chats(4, 'Olivia2','http://placehold.it/45','10:00AM','hello this is test message'),
+    new Chats(5, 'Windstorm','http://placehold.it/45','10:00AM','hello this is test message'),
+    new Chats(6, 'Princess Amelia','http://placehold.it/45','10:00AM','hello this is test message'),
+    new Chats(6, 'Olivia1','http://placehold.it/45','10:00AM','hello this is test message')
+  ]
   ngOnInit() {
   }
 
--- chat-app/src/app/contact-list/contact-list.component.ts
+++ chat-app/src/app/contact-list/contact-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Contact} from '../contact';
 
 @Component({
   selector: 'app-contact-list',
@@ -8,7 +9,15 @@ import { Component, OnInit } from '@angular/core';
 export class ContactListComponent implements OnInit {
 
   constructor() { }
-
+  myContact = [
+    new Contact(1, 'Olivia','http://placehold.it/45'),
+    new Contact(2, 'Jake Oliver','http://placehold.it/45'),
+    new Contact(3, 'Windstorm','http://placehold.it/45'),
+    new Contact(4, 'Olivia2','http://placehold.it/45'),
+    new Contact(5, 'Windstorm','http://placehold.it/45'),
+    new Contact(6, 'Princess Amelia','http://placehold.it/45'),
+    new Contact(6, 'Olivia1','http://placehold.it/45')
+  ]
   ngOnInit() {
   }
 
