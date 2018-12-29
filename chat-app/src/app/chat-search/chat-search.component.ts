42c78fc9c751e7ac2299421eedb7ed24bb2a5246
Eslam Saeed <Eslamsa3ed50@gmail.com>
2018-12-29 21:01:48 +0200

    build fix

--- chat-app/src/app/chat-search/chat-search.component.ts
+++ chat-app/src/app/chat-search/chat-search.component.ts
@@ -1,28 +1,24 @@
-import { Component, OnInit } from '@angular/core';
-import { Contact } from '../contact';
-import { Type } from '@angular/compiler/src/output/output_ast';
+import { Component, OnInit } from "@angular/core";
+import { Contact } from "../contact";
+import { Type } from "@angular/compiler/src/output/output_ast";
 
 @Component({
-  selector: 'app-chat-search',
-  templateUrl: './chat-search.component.html',
-  styleUrls: ['./chat-search.component.css']
+  selector: "app-chat-search",
+  templateUrl: "./chat-search.component.html",
+  styleUrls: ["./chat-search.component.css"]
 })
 export class ChatSearchComponent implements OnInit {
+  constructor() {}
+  searchText = "";
 
-  constructor() { 
-   
-  }
- 
   Contacts = [
-      'Olivia',
-      'Jake Oliver',
-      'Princess Amelia',
-      'Lumpy Harry',
-      'Olivia1',
-      'Lumpy Harry',
-      'Olivia2'
-    ]
-  ngOnInit() {
-  }
-
+    "Olivia",
+    "Jake Oliver",
+    "Princess Amelia",
+    "Lumpy Harry",
+    "Olivia1",
+    "Lumpy Harry",
+    "Olivia2"
+  ];
+  ngOnInit() {}
 }
