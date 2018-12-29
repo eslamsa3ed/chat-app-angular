66c0f38a552fe6deaf6aefb3eb16a486d3776d12
Eslam Saeed <Eslamsa3ed50@gmail.com>
2018-12-29 20:45:51 +0200

    build fix

--- chat-app/src/app/contact-list/contact-list.component.ts
+++ chat-app/src/app/contact-list/contact-list.component.ts
@@ -1,24 +1,21 @@
-import { Component, OnInit } from '@angular/core';
-import {Contact} from '../contact';
+import { Component, OnInit } from "@angular/core";
+import { Contact } from "../contact";
 
 @Component({
-  selector: 'app-contact-list',
-  templateUrl: './contact-list.component.html',
-  styleUrls: ['./contact-list.component.css']
+  selector: "app-contact-list",
+  templateUrl: "./contact-list.component.html",
+  styleUrls: ["./contact-list.component.css"]
 })
 export class ContactListComponent implements OnInit {
-
-  constructor() { }
+  constructor() {}
   myContact = [
-    new Contact(1, 'Olivia','http://placehold.it/45'),
-    new Contact(2, 'Jake Oliver','http://placehold.it/45'),
-    new Contact(3, 'Windstorm','http://placehold.it/45'),
-    new Contact(4, 'Olivia2','http://placehold.it/45'),
-    new Contact(5, 'Windstorm','http://placehold.it/45'),
-    new Contact(6, 'Princess Amelia','http://placehold.it/45'),
-    new Contact(6, 'Olivia1','http://placehold.it/45')
-  ]
-  ngOnInit() {
-  }
-
+    new Contact(1, "Olivia", "http://placehold.it/45"),
+    new Contact(2, "Jake Oliver", "http://placehold.it/45"),
+    new Contact(3, "Windstorm", "http://placehold.it/45"),
+    new Contact(4, "Olivia2", "http://placehold.it/45"),
+    new Contact(5, "Windstorm", "http://placehold.it/45"),
+    new Contact(6, "Princess Amelia", "http://placehold.it/45"),
+    new Contact(6, "Olivia1", "http://placehold.it/45")
+  ];
+  ngOnInit() {}
 }
